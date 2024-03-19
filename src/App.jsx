2fff82e4177d86695c5ce38dadfda4b393f198ad
juligarc91 <julissagarcia@pursuit.org>
2024-03-19 14:50:13 -0400

    installed browser router and removed dotenv from frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>Tunr Frontend</div>;
+  return (
+    <main>
+      <Routes>
+        
+      </Routes>
+    </main>
+  )
 };
 
 export default App;
