2245bb73d74fefbe59942611022ba34d3dee72c9
juligarc91 <julissagarcia@pursuit.org>
2024-03-19 14:59:06 -0400

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ const App = () => {
   return (
     <main>
       <Routes>
-        
+        <Route path="*" element=
+          {<h1>404: Page Not Found</h1>}/>
+        <Route path="/" element=
+          {<h1>Home Page</h1>}/>
+        <Route path="/songs" element=
+          {<h1>index</h1>}/>
+        <Route path="/songs/new" element=
+          {<h1>New - Create Form</h1>}/>
+        <Route path="/songs/:id" element=
+          {<h1>Show One</h1>}/>
+        <Route path="/bookmarks/:id/edit" element=
+          {<h1>Edit Form</h1>}/>
       </Routes>
     </main>
   )
